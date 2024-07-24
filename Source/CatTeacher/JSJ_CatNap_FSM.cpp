7b0252c8ed25e0566888effe631864f67de3a896
ryi01 <kkjmo3@naver.com>
2024-07-24 09:29:03 +0900

    JSH StateMachine

---
+++ Source/CatTeacher/JSJ_CatNap_FSM.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JSJ_CatNap_FSM.h"
+
+// Sets default values for this component's properties
+UJSJ_CatNap_FSM::UJSJ_CatNap_FSM()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UJSJ_CatNap_FSM::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UJSJ_CatNap_FSM::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+
+#pragma region FSM
+	// m_state에 따른 변화
+	switch (cState)
+	{
+	case CATNAP_State::C_Idle:
+		IdleState();
+		break;
+	case CATNAP_State::C_Move:
+		MoveState();
+		break;
+		CATNAP_State
+	case EBossState::C_Attack:
+		AttackState();
+		break;
+	}
+#pragma endregion
+}
+
+#pragma region IdleState
+void UJSJ_CatNap_FSM::IdleState()
+{
+
+}
+#pragma endregion
+
+
+#pragma region MoveState
+void UJSJ_CatNap_FSM::MoveState()
+{
+
+}
+#pragma endregion
+
+#pragma region AttackState
+void UJSJ_CatNap_FSM::AttackState()
+{
+
+}
+#pragma endregion
+
---
+++ Source/CatTeacher/JSJ_CatNap_FSM.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "JSJ_CatNap_FSM.generated.h"
+// State 변수들
+UENUM()
+enum class CATNAP_State : uint8
+{
+	C_Idle,
+	C_Move,
+	C_Attack,
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CATTEACHER_API UJSJ_CatNap_FSM : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UJSJ_CatNap_FSM();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// 스테이트 enum 변수를 쓰기 위한 변수
+	CATNAP_State cState = CATNAP_State::C_Idle;
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+#pragma region 	StateFunction
+	// 상태에 따른 함수들
+	// 대기 상태
+	void IdleState();
+	// 움직이는 상태
+	void MoveState();
+	// Attack 상태
+	void AttackState();
+	
+#pragma endregion
+
+	
+
+		
+};

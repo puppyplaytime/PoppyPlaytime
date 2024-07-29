482a2274e9324ea72e9d24229e014f44fc0068b6
ghkgus407 <143527072+ghkgus407@users.noreply.github.com>
2024-07-29 16:13:53 +0900

    teacher

---
+++ Source/CatTeacher/Private/KHH_Enemy.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KHH_Enemy.h"
+#include "KHH_EnemyFSM.h"
+
+
+// Sets default values
+AKHH_Enemy::AKHH_Enemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh>tempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequin_UE4/Meshes/SK_Mannequin.SK_Mannequin'"));
+
+	if (tempMesh.Succeeded()) {
+		GetMesh()->SetSkeletalMesh(tempMesh.Object);
+
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -88), FRotator(0, -90, 0));
+	}
+	fsm = CreateDefaultSubobject<UKHH_EnemyFSM>(TEXT("FSM"));
+
+}
+
+// Called when the game starts or when spawned
+void AKHH_Enemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AKHH_Enemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AKHH_Enemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/CatTeacher/Private/KHH_OpenDoor.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KHH_OpenDoor.h"
+#include "Math/UnrealMathUtility.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
+#include "Engine/World.h"
+#include "Components/ActorComponent.h"
+
+// Sets default values for this component's properties
+UKHH_OpenDoor::UKHH_OpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UKHH_OpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	OriginalLocation = GetOwner()->GetActorLocation();
+	
+}
+
+
+// Called every frame
+void UKHH_OpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!Player) return;
+
+	// 플레이어와 문의 거리 계산
+	FVector DistanceVector = Player->GetActorLocation() - GetOwner()->GetActorLocation();
+	float Distance = DistanceVector.Length();
+
+	// 문을 열어야 할 때
+	if (Distance <= DistanceThreshold && !ShouldMove)
+	{
+		MoveDoor(DeltaTime);
+	}
+}
+
+void UKHH_OpenDoor::MoveDoor(float DeltaTime)
+{
+	CurrentLocation = GetOwner()->GetActorLocation();
+	TargetLocation = OriginalLocation + MoveOffset;
+	Speed = FVector::Distance(OriginalLocation, TargetLocation) / MoveTime;
+	NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+	GetOwner()->SetActorLocation(NewLocation);
+}
+
+
